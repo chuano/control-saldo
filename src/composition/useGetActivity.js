9a2146f8eb2b84ebd92534f28247a07f4f7aeb51
Chuano Puchol <chuano@gmail.com>
2020-09-03 00:22:24 +0200

    Go to activity list if activity not found

--- src/composition/useGetActivity.js
+++ src/composition/useGetActivity.js
@@ -1,12 +1,17 @@
 import { useStore } from 'vuex'
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default function useGetActivity(id) {
+  const router = useRouter()
   const store = useStore()
   const activity = computed(() => {
     if (id) {
-      return store.state.activities.activities.find((x) => x.id === parseInt(id))
+      const activity = store.state.activities.activities.find((x) => x.id === parseInt(id))
+      if (activity) return activity
+      router.replace({ name: 'ActivityListView' })
     }
+
     return {
       id: 0,
       name: null,
