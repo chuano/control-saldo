f8c0f9b83c4fab21813c55293cfcc7e9e18cc47f
Chuano Puchol <chuano@gmail.com>
2020-09-12 01:01:01 +0200

    Credit charges ordered by descending date

--- src/composition/useGetActivityCreditChanges.js
+++ src/composition/useGetActivityCreditChanges.js
@@ -1,9 +1,12 @@
 import { computed } from 'vue'
+import { compareProperty } from '../util/compareProperty'
 
 export default function useGetActivityCreditChanges(activity, year, limit) {
-  const changes = computed(() =>
-    activity.changes.filter((c) => new Date(c.date).getFullYear() === year).slice(0, limit),
-  )
+  const changes = computed(() => {
+    const yearChanges = activity.changes.filter((c) => new Date(c.date).getFullYear() === year)
+    yearChanges.sort(compareProperty('date', 'desc'))
+    return yearChanges.slice(0, limit)
+  })
 
   return { changes }
 }
