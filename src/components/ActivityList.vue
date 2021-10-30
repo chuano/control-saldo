b9af289f1013828787024a76660a5de2c7d467de
Chuano <chuano@gmail.com>
2021-10-30 18:36:14 +0200

    Sortable activities

--- src/components/ActivityList.vue
+++ src/components/ActivityList.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="row">
+  <draggable class="dragArea list-group w-full" :list="activities" @change="saveOrder">
     <div v-for="activity in activities" :key="activity.id">
       <button class="block" @click="$emit('selection', activity)">
         {{ activity.name }}
       </button>
     </div>
-    <div>
-      <button class="block" v-if="addButton" @click="$router.push({ name: 'NewActivityView' })">
-        <img src="../assets/add-white-36dp.svg" alt="Add" />
-      </button>
-    </div>
-  </div>
-  <div class="row">
-    <div>
-      <button class="block-inline" @click="makeBackup">Guardar copia en la nube</button>
-    </div>
-    <div>
-      <button class="block-inline" @click="restoreBackup">Recuperar copia de la nube</button>
-    </div>
+  </draggable>
+  <div>
+    <button class="block" v-if="addButton" @click="$router.push({ name: 'NewActivityView' })">
+      <img src="../assets/add-white-36dp.svg" alt="Add" />
+    </button>
+    <button class="block blue" @click="makeBackup">Guardar copia en la nube</button>
+    <button class="block blue" @click="restoreBackup">Recuperar copia de la nube</button>
   </div>
 </template>
 
 <script>
+import { useStore } from 'vuex'
 import useBackup from '../composition/useBackup'
 import useGetActivities from '../composition/useGetActivities'
 import useRestore from '../composition/useRestore'
+import { VueDraggableNext } from 'vue-draggable-next'
 
 export default {
   name: 'ActiviyList',
+  components: {
+    draggable: VueDraggableNext,
+  },
   props: {
     addButton: {
       tyoe: Boolean,
@@ -38,31 +37,26 @@ export default {
     const { activities } = useGetActivities()
     const { makeBackup } = useBackup()
     const { restoreBackup } = useRestore()
+    const store = useStore()
+    const saveOrder = () => {
+      store.commit('setActivities', activities.value)
+    }
 
     return {
       activities,
       makeBackup,
       restoreBackup,
+      saveOrder,
     }
   },
 }
 </script>
 
 <style scoped>
-.row {
-  width: calc(100% + 16px);
-  margin-left: -8px;
-}
-.row div {
-  width: calc(50% - 16px);
-  padding: 8px;
-}
 button.block {
-  height: 40vw;
+  margin-bottom: 8px;
 }
-button.block-inline {
-  width: 100%;
-  height: 20vw;
+button.blue {
   background-color: dodgerblue;
 }
 </style>
--- src/util/storageSubscriber.js
+++ src/util/storageSubscriber.js
@@ -1,5 +1,6 @@
 export default function storageSubscriber(store) {
   store.subscribe((mutation, state) => {
+    console.log("SI")
     localStorage.setItem('activities', JSON.stringify(state.activities.activities))
   })
 }
